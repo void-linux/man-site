b678b6ddbd81e5f1a8d390491c7caa9b735c6b50
Michael Aldridge <aldridge.mac@gmail.com>
2021-04-18 02:21:47 -0700

    xmandump: Ensure all manpaths exist

--- xmandump/update.sh
+++ xmandump/update.sh
@@ -3,6 +3,7 @@
 mkdir -p /man-cgi && cd /man-cgi || exit 1
 
 for arch in x86_64 musl/x86_64-musl armv7l musl/armv7l-musl; do
+    mkdir -p ./${arch#musl/} || exit 1
     cd ./${arch#musl/} || exit 1
     xmandump -c cache.json /pkgs/${arch}-repodata
     cd ../ || exit 1
